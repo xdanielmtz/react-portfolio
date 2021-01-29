506bdc15bdc9356e34c8fe0c972d5795e71e2b57
Dani JM <xdanielmtz@gmail.com>
2021-01-29 00:48:09 -0500

    added hard coded data to be diasplayed to the screen

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            This is my Contact Page
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div>
+            This is my portfolio Page.
+        </div>
+    );
+};
+
+export default Portfolio;
