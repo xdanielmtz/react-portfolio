8b44c759677b020ebcb2fff54dc8b52d5550a6bb
Dani JM <xdanielmtz@gmail.com>
2021-01-29 01:34:51 -0500

    added routes to my pages in the appjs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+import Portfolio from "./pages/Portfolio/Portfolio";
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
