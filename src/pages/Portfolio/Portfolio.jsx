3bac5020333cd2f71edf52ec7d78173aada1c037
Daniel Martinez <xdanielmtz@gmail.com>
2021-01-30 17:11:53 -0500

    added one project to display in the portfolio page

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,11 +1,81 @@
-import React from 'react';
+import React from "react";
+import empDir from "./employee-directory-ss.png" 
+import "./portfolio.css"
 
 const Portfolio = () => {
-    return (
-        <div>
-            This is my portfolio Page.
+  return (
+    <>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+      <div className="container">
+        <div className="row">
+          <div className="col s6">
+              <h3 style={{ fontWeight: "bold" }}>Employee Directory</h3>
+              <br/>
+              <a className="linkTag" href="https://xdanielmtz.github.io/employee-directory/">Deployed Site</a>
+              <br/>
+              <br/>
+              <br/>
+              <a className="linkTag" href="https://xdanielmtz.github.io/employee-directory/">Repository Page</a>
+          </div>
+          <div className="col s6">
+              <img src={empDir} alt="blah" height="500px" style={{border:"1px solid #000000"}} />
+          </div>
         </div>
-    );
+      </div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <div className="container">
+        <div className="row">
+          <div className="col s6">
+              <h3 style={{ fontWeight: "bold" }}>Employee Directory</h3>
+              <br/>
+              <a className="linkTag" href="https://xdanielmtz.github.io/employee-directory/">Deployed Site</a>
+              <br/>
+              <br/>
+              <br/>
+              <a className="linkTag" href="https://xdanielmtz.github.io/employee-directory/">Repository Page</a>
+          </div>
+          <div className="col s6">
+              <img src={empDir} alt="blah" height="500px" style={{border:"1px solid #000000"}} />
+          </div>
+        </div>
+      </div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <div className="container">
+        <div className="row">
+          <div className="col s6">
+              <h3 style={{ fontWeight: "bold" }}>Employee Directory</h3>
+              <br/>
+              <a className="linkTag" href="https://xdanielmtz.github.io/employee-directory/">Deployed Site</a>
+              <br/>
+              <br/>
+              <br/>
+              <a className="linkTag" href="https://xdanielmtz.github.io/employee-directory/">Repository Page</a>
+          </div>
+          <div className="col s6">
+              <img src={empDir} alt="blah" height="500px" style={{border:"1px solid #000000"}} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Portfolio;
