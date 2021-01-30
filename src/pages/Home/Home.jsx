d85b0234e3e33baae7a22524b84fe5259590a42f
Dani JM <xdanielmtz@gmail.com>
2021-01-29 22:55:27 -0500

    updating css in home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
+import "./home.css";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>
-                This is my HomePage
-            </h1>
+  return (
+    <>
+      <div id="homeContainer">
+        <div className="container">
+          <div className="row">
+            <div className="col s3">img here</div>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Home;
