58beb9f226fd6ff1175bd38c34b74cf9067640e7
Daniel Martinez <xdanielmtz@gmail.com>
2021-01-30 18:14:06 -0500

    finished contact page.

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import "./contact.css";
+import resume from "./Resume.pdf";
 
 const Contact = () => {
     return (
-        <div>
-            This is my Contact Page
-        </div>
+        <>
+        <br/>
+        <br/>
+            <div className="container">
+                <div className="row">
+                    <div className="col s12 center-align">
+                        <h1 style={{ fontWeight: "bold" }}>
+                            Contact Me!
+                        </h1>
+                        <ul>
+                        <li>
+                          <a className="theLinks" href="https://github.com/xdanielmtz">Github</a>
+                        </li>
+                        <li>
+                          <a className="theLinks" href="https://www.linkedin.com/in/daniel-martinez-2130b81b5/">LinkdIn</a>
+                        </li>
+                        <li>
+                          <a className="theLinks" href={resume} target="blank">Resume</a>
+                        </li>
+                        <li>
+                            <p className="myInfo"> 678-650-4147</p>
+                            <p className="myInfo">xdanielmtz@gmail.com</p>
+                        </li>
+                     </ul>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -24,7 +24,7 @@ const Portfolio = () => {
               <a className="linkTag" href="https://xdanielmtz.github.io/employee-directory/">Repository Page</a>
           </div>
           <div className="col s6">
-              <img src={empDir} alt="blah" height="500px" style={{border:"1px solid #000000"}} />
+              <img src={empDir} alt="blah" height="500px" width="800px" style={{border:"1px solid #000000"}} />
           </div>
         </div>
       </div>
@@ -48,7 +48,7 @@ const Portfolio = () => {
               <a className="linkTag" href="https://github.com/xdanielmtz/Project-1">Repository Page</a>
           </div>
           <div className="col s6">
-              <img src={safeMusic} alt="blah" height="500px" style={{border:"1px solid #000000"}} />
+              <img src={safeMusic} alt="blah" height="500px" width="800px" style={{border:"1px solid #000000"}} />
           </div>
         </div>
       </div>
@@ -73,7 +73,7 @@ const Portfolio = () => {
               <a className="linkTag" href="https://github.com/xdanielmtz/football-or-futbol">Repository Page</a>
           </div>
           <div className="col s6">
-              <img src={footOrFut} alt="blah" height="500px" style={{border:"1px solid #000000"}} />
+              <img src={footOrFut} alt="blah" height="500px" width="800px" style={{border:"1px solid #000000"}} />
           </div>
         </div>
       </div>
