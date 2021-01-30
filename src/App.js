b623f4f7f4880e0550aba980ea84df4d96281435
Dani JM <xdanielmtz@gmail.com>
2021-01-29 21:59:53 -0500

    brought in navbar from materialize. placed it on all routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Contact from "./pages/Contact/Contact";
 import Home from "./pages/Home/Home";
 import Portfolio from "./pages/Portfolio/Portfolio";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Router>
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/Home" component={Home} />
