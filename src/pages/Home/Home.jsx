0bca389c3a6d6d50fdf5e47aa1299aa1040e179b
Daniel Martinez <xdanielmtz@gmail.com>
2021-02-20 16:48:27 -0500

    edits in home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
         is a quick overview <br /> of my main technical skill sets and tools I
         have used. Want to find out more about my experience?{" "}
         <a href="https://docs.google.com/document/d/1Y032gC5r-Xg9yC9JL9LPpukUcwriwOar9rTpsvwfkt4/edit">
-          Check out my resume{" "}
+          <strong>Check out my resume.{" "}</strong>
         </a>
       </p>
       <br />
