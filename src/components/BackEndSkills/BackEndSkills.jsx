949b9d7b1a8a21a0ae809020e63624396b405419
Daniel Martinez <xdanielmtz@gmail.com>
2021-02-21 17:29:51 -0500

    added a back end skills component

--- src/components/BackEndSkills/BackEndSkills.jsx
+++ src/components/BackEndSkills/BackEndSkills.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const BackEndSkills = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <>
+      <div className="col s4 center-align">
+        <i className="large material-icons">open_in_browser</i>
+        <h5 style={{ fontWeight: "bold" }}>Back End</h5>
+        <ul>
+          <li>Templating Engines</li>
+          <li>Sessions</li>
+          <li>Writing Tests</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>Creating APIs</li>
+          <li>MVC</li>
+          <li>ORM (Sequelize)</li>
+          <li>MySQL</li>
+          <li>MongoDB</li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default BackEndSkills;
