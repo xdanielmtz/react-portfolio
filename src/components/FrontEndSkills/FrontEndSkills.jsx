a9ca47aed0bb8fab2277e3ae3bd42261bc58928a
Daniel Martinez <xdanielmtz@gmail.com>
2021-02-21 17:27:49 -0500

    more refacroting. added a front end skills component

---
+++ src/components/FrontEndSkills/FrontEndSkills.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const FrontEndSkills = () => {
+  return (
+    <>
+      <div className="col s4 center-align">
+        <i className="large material-icons">tab</i>
+        <h5 style={{ fontWeight: "bold" }}>Front End</h5>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>Bootstrap</li>
+          <li>React.js</li>
+          <li>APIs</li>
+          <li>JSON</li>
+          <li>AJAX</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default FrontEndSkills;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,21 +10,7 @@ const Home = () => {
       <HomeOverview />
       <div className="container">
         <div className="row">
-          <div className="col s4 center-align">
-            <i className="large material-icons">tab</i>
-            <h5 style={{ fontWeight: "bold" }}>Front End</h5>
-            <ul>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>jQuery</li>
-              <li>Bootstrap</li>
-              <li>React.js</li>
-              <li>APIs</li>
-              <li>JSON</li>
-              <li>AJAX</li>
-            </ul>
-          </div>
+          
           <div className="col s4 center-align">
             <i className="large material-icons">open_in_browser</i>
             <h5 style={{ fontWeight: "bold" }}>Back End</h5>
