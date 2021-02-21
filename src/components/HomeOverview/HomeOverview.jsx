92db74aee64065c2f52a13cc4f291021c1382b4e
Daniel Martinez <xdanielmtz@gmail.com>
2021-02-21 17:23:26 -0500

    added an overview component

---
+++ src/components/HomeOverview/HomeOverview.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HomeOverview = () => {
+  return (
+    <>
+      <h4 className="center-align">Skills Overview</h4>
+      <p className="center-align">
+        I have more than 6 months experience building rich applications. Below
+        is a quick overview <br /> of my main technical skill sets and tools I
+        have used. Want to find out more about my experience?{" "}
+        <a href="https://docs.google.com/document/d/1Y032gC5r-Xg9yC9JL9LPpukUcwriwOar9rTpsvwfkt4/edit">
+          <strong>Check out my resume. </strong>
+        </a>
+      </p>
+      <br />
+    </>
+  );
+};
+
+export default HomeOverview;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,16 +7,7 @@ const Home = () => {
   return (
     <>
      <Banner/>
-      <h4 className="center-align">Skills Overview</h4>
-      <p className="center-align">
-        I have more than 6 months experience building rich applications. Below
-        is a quick overview <br /> of my main technical skill sets and tools I
-        have used. Want to find out more about my experience?{" "}
-        <a href="https://docs.google.com/document/d/1Y032gC5r-Xg9yC9JL9LPpukUcwriwOar9rTpsvwfkt4/edit">
-          <strong>Check out my resume.{" "}</strong>
-        </a>
-      </p>
-      <br />
+   
       <div className="container">
         <div className="row">
           <div className="col s4 center-align">
