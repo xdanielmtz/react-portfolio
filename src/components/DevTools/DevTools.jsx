ff1b24b71f73f6e4feafbb4fba936f49c3312076
Daniel Martinez <xdanielmtz@gmail.com>
2021-02-21 17:34:51 -0500

    added a devt tools componet

---
+++ src/components/DevTools/DevTools.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const DevTools = () => {
+  return (
+    <>
+      <div className="col s4 center-align">
+        <i className="large material-icons">person</i>
+        <h5 style={{ fontWeight: "bold" }}>Dev Tools</h5>
+        <ul>
+          <li>Agile Methodology</li>
+          <li>Heroku</li>
+          <li>Git</li>
+          <li>Github</li>
+          <li>Figma</li>
+          <li>Robo 3T</li>
+          <li>MongoDB Atlas</li>
+          <li>PostMan</li>
+          <li>Kanban Board</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default DevTools;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,21 +14,7 @@ const Home = () => {
         <div className="row">
           <FrontEndSkills />
           <BackEndSkills />
-          <div className="col s4 center-align">
-            <i className="large material-icons">person</i>
-            <h5 style={{ fontWeight: "bold" }}>Dev Tools</h5>
-            <ul>
-              <li>Agile Methodology</li>
-              <li>Heroku</li>
-              <li>Git</li>
-              <li>Github</li>
-              <li>Figma</li>
-              <li>Robo 3T</li>
-              <li>MongoDB Atlas</li>
-              <li>PostMan</li>
-              <li>Kanban Board</li>
-            </ul>
-          </div>
+         
         </div>
       </div>
     </>
