238b25424441e79f5100b7e773e1fd9128425cbe
Daniel Martinez <xdanielmtz@gmail.com>
2021-02-21 20:22:02 -0500

    css changes all round

--- src/components/HomeOverview/HomeOverview.jsx
+++ src/components/HomeOverview/HomeOverview.jsx
@@ -3,16 +3,22 @@ import React from "react";
 const HomeOverview = () => {
   return (
     <>
-      <h4 className="center-align">Skills Overview</h4>
-      <p className="center-align">
-        I have more than 6 months experience building rich applications. Below
-        is a quick overview <br /> of my main technical skill sets and tools I
-        have used. Want to find out more about my experience?{" "}
-        <a href="https://docs.google.com/document/d/1Y032gC5r-Xg9yC9JL9LPpukUcwriwOar9rTpsvwfkt4/edit">
-          <strong>Check out my resume. </strong>
-        </a>
-      </p>
-      <br />
+      <div className="container containerOverview">
+        <div className="row">
+          <div className="col s12 columnOverview">
+            <h4 className="center-align">Skills Overview</h4>
+            <p className="center-align">
+              I have more than 6 months experience building rich applications.
+              Below is a quick overview <br /> of my main technical skill sets
+              and tools I have used. Want to find out more about my experience?{" "}
+              <a href="https://docs.google.com/document/d/1Y032gC5r-Xg9yC9JL9LPpukUcwriwOar9rTpsvwfkt4/edit">
+                <strong>Check out my resume. </strong>
+              </a>
+            </p>
+            <br />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,24 +9,48 @@ import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <div class="nav-wrapper">
-          <NavLink to="/home" className="navName">Daniel Martinez</NavLink>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li>
-              <NavLink to="/home" activeStyle={{backgroundColor: "rgba(0,0,0,0.1)"}}>Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/portfolio" activeStyle={{backgroundColor: "rgba(0,0,0,0.1)"}}>Portfolio</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact" activeStyle={{backgroundColor: "rgba(0,0,0,0.1)"}}>Contact</NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+    <>
+      <div>
+        <nav>
+          <div class="nav-wrapper">
+            <div className="container">
+              <div className="row">
+                <NavLink to="/home" className="navName">
+                  Daniel Martinez
+                </NavLink>
+
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                  <li>
+                    <NavLink
+                      to="/home"
+                      activeStyle={{ backgroundColor: "rgba(0,0,0,0.1)" }}
+                    >
+                      Home
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/portfolio"
+                      activeStyle={{ backgroundColor: "rgba(0,0,0,0.1)" }}
+                    >
+                      Portfolio
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/contact"
+                      activeStyle={{ backgroundColor: "rgba(0,0,0,0.1)" }}
+                    >
+                      Contact
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
   );
 };
 
